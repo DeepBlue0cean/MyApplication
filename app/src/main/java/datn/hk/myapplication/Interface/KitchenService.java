7e8a3c29e67d1f5fe77fbfc3bf38deb903d8e22a
DeepBlue0cean <39375067+DeepBlue0cean@users.noreply.github.com>
2019-06-15 17:20:22 +0700

    =

--- app/src/main/java/datn/hk/myapplication/Interface/KitchenService.java
+++ app/src/main/java/datn/hk/myapplication/Interface/KitchenService.java
@@ -1,13 +1,13 @@
 package datn.hk.myapplication.Interface;
 
-import datn.hk.myapplication.Activity.KitchenActivity_ViewBinding;
+import datn.hk.myapplication.databinding.ActivityKitchenBinding;
 import datn.hk.myapplication.models.Kitchen;
 
 
 public abstract class KitchenService {
-    public static Kitchen kitchen = new Kitchen();
+
     public final String LOCATE = "Kitchen";
     public final String PATH = "home";
 
-    public abstract Kitchen getInfo(String path, String locate);
+    public abstract Kitchen getInfo(ActivityKitchenBinding activityKitchenBinding,String path, String locate);
 }
